d84b0074add6f108a08712235e71c7ea1c5419c0
Emily <emily.zy.lin@gmail.com>
2024-10-15 09:26:38 -0500

    studio basic problem is solved

--- Chapter2/Chapter2Studio/Chapter2Studio/Program.cs
+++ Chapter2/Chapter2Studio/Chapter2Studio/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Diagnostics.Metrics;
+
+Console.WriteLine("Enter a radius: ");
+
+string input = Console.ReadLine();
+
+float radius;
+
+radius = float.Parse(input);
+
+double result = radius * radius * 3.14;
+
+Console.WriteLine("The area of a circle of radius " + radius + " is: " + result);
+
