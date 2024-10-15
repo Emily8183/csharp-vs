86307380172963889d982035aebdb20d8744821f
Emily <emily.zy.lin@gmail.com>
2024-10-15 14:33:56 -0500

    add exception handle

--- Chapter2/Chapter2Studio/Chapter2Studio/Program.cs
+++ Chapter2/Chapter2Studio/Chapter2Studio/Program.cs
@@ -4,42 +4,63 @@ using System.Diagnostics.Metrics;
 
 using Chapter2Studio;
 
-Boolean stopLoop = false;
+//Solution3: do while
 
-while (!stopLoop)
+float radius;
+
+do
 {
-    Console.WriteLine("Enter a radius: ");
+    Console.WriteLine("Please enter a positive radius");
+    string input = Console.ReadLine();
 
-    try
+    if (Equals(input, "") || !float.TryParse(input, out radius))
     {
-        string input = Console.ReadLine();
-
-        float radius = float.Parse(input);
-
-        if (radius > 0)
-        {
-            double result = Circles.CalculateRadius(radius);
-            Console.WriteLine("The area of a circle of radius " + radius + " is: " + result);
-            stopLoop = true;
-        }
-        else
-        {
-            Console.WriteLine("Enter a valid number for the radius: ");
-        }
+        Console.WriteLine("I can't calculate it");
+        return;
     }
+    //radius = float.Parse(input);
 
-    catch(FormatException)
-    {
-        Console.WriteLine("You did not enter a valid format.");
-    }
+} while (radius < 0);
 
 
-  
-}
+double result = Circles.CalculateRadius(radius);
+Console.WriteLine("The area of a circle of radius " + radius + " is: " + result);
+
+//Solution2: try-catch
+
+//Boolean stopLoop = false;
+
+//while (!stopLoop)
+//{
+//    Console.WriteLine("Enter a radius: ");
+
+//    try
+//    {
+//        string input = Console.ReadLine();
+
+//        float radius = float.Parse(input);
+
+//        if (radius > 0)
+//        {
+//            double result = Circles.CalculateRadius(radius);
+//            Console.WriteLine("The area of a circle of radius " + radius + " is: " + result);
+//            stopLoop = true;
+//        }
+//        else
+//        {
+//            Console.WriteLine("Enter a valid number for the radius: ");
+//        }
+//    }
+
+//    catch(FormatException)
+//    {
+//        Console.WriteLine("You did not enter a valid format.");
+//    }  
+//}
 
 
 
-//basic code to solve the studio problem
+//Solution 1: basic code to solve the studio problem
 //Console.WriteLine("Enter a radius: ");
 
 //string input = Console.ReadLine();
