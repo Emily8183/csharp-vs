69b47dd35c2dbf0e19eb55a5f0024d4ebc5ac30f
Emily <emily.zy.lin@gmail.com>
2024-10-16 21:32:30 -0500

    test new menuitem

--- Chapter4and5/Program.cs
+++ Chapter4and5/Program.cs
@@ -1,6 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 using Chapter4and5;
 
 MenuItem menuitem1 = new MenuItem("burger", 13, "Vegan", "main");
 
-Console.WriteLine(menuitem1);//added override ToString to print out this object from the MenuItem class
+//Console.WriteLine(menuitem1.Name);
+//can directly print out in this way
+
+//Console.WriteLine(menuitem1);
+//added override ToString to print out this object from the MenuItem class
