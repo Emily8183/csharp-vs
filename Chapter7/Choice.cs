3f45fbf6795b8e49b01528f0f8a5fad8fc1f537b
Emily <emily.zy.lin@gmail.com>
2024-10-17 22:36:51 -0500

    add Choice class

---
+++ Chapter7/Choice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter7
+{
+    public class Choice
+    {
+        public string Content {  get; set; }
+        public bool IsCorrect {  get; set; }
+
+        public Choice(string content, bool isCorrect)
+        {
+            Content = content;
+            IsCorrect = isCorrect;
+        }
+
+        public Choice(string content)
+            : this(content, false) { }
+    }
+}
